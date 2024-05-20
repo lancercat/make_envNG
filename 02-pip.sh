93f65c1fab17cd4aae2237f5dd87ceb63a6c6164
lancercat <lasercat@gmx.us>
2024-05-20 12:10:13 +0200

    Update 02-pip.sh

--- 02-pip.sh
+++ 02-pip.sh
@@ -1,7 +1,7 @@
 rm build -rf
 mkdir build
 pip install accelerate bs4 easydict flwr llama_index_core llama_index_embeddings_huggingface llama_index_llms_huggingface
-easydict timm scikit-image --user --break-system-packages
+easydict timm scikit-image wandb --user --break-system-packages
 sh torch_scatter.sh 
 #sh torch_vision.sh 
 sh pylcs.sh 
