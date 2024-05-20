80e48a0a0144c8604ebdf2e853692ce5cc4339e0
lancercat <lasercat@gmx.us>
2024-05-20 10:22:24 +0200

    Update 02-pip.sh
    Some weird stuff mixed in. Don't mind ;-)

--- 02-pip.sh
+++ 02-pip.sh
@@ -1,6 +1,7 @@
 rm build -rf
 mkdir build
-pip install easydict --user --break-system-packages
+pip install accelerate bs4 easydict flwr llama_index_core llama_index_embeddings_huggingface llama_index_llms_huggingface
+easydict timm scikit-image --user --break-system-packages
 sh torch_scatter.sh 
 #sh torch_vision.sh 
 sh pylcs.sh 
